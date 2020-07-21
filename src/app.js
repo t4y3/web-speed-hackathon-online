9ce17a3bc5e0a74aec498aaeecb0d634ad4e274f
t4y3 <y.teraoka8@gmail.com>
2020-07-22 00:27:42 +0900

    Fix: 微修正 🐛

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 import './foundation/polyfills';
 
 import { render } from './foundation/render';
-import { setupMockAPIData } from './foundation/gateway';
 
-function init() {
-  if (process.env.USE_MOCK_DATA === 'true') {
-    setupMockAPIData();
-  }
-
+window.addEventListener('DomContentLoaded', () => {
   render();
-}
-
-window.onload = () => {
-  init();
-};
+});
