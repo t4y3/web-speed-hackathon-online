5f85e898e6e338d7fe4f14291faf5ba0106b637a
t4y3 <y.teraoka8@gmail.com>
2020-07-22 13:33:42 +0900

    Fix: http->https 🐛

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,5 +1,5 @@
 const API_PATH = SERVICE_ENV === 'local' ? 'http://localhost:9000' : '';
 const ASSETS_PATH = 'https://ryotasugawara.github.io/assets/';
-const CDN_ASSETS_PATH = 'http://web-speed-hackathon-online.imgix.net/';
+const CDN_ASSETS_PATH = 'https://web-speed-hackathon-online.imgix.net/';
 
 export { API_PATH, ASSETS_PATH, CDN_ASSETS_PATH };
